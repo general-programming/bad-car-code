ba2a04b3eafc0ae01126bd8522ef89e49b5bc8c8
nepeat <nepeat@users.noreply.github.com>
2018-11-07 15:58:38 -0800

    Bus flooder.

---
+++ zeroxzero.py
@@ -0,0 +1,9 @@
+from panda import Panda
+
+panda = Panda()
+panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+
+while True:
+    for bus in range(0, 2):
+        panda.can_send(0x0, os.urandom(8), bus)
+
